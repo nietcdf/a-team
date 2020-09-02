c26568ee0d0d1518f79ca92d8ab4bfa073c2296b
Rafal Jabrzemski <rjabrzemski@ou.edu>
2020-09-02 16:08:00 -0500

    Initial commit

---
+++ Driver.java
@@ -0,0 +1,23 @@
+
+public class Driver {
+
+	public static void main(String[] args) {
+		int x = 5;
+		System.out.println("Fixin' to go to the PL");
+
+		// create info about a triangle
+		int base = 1;
+		int height = 2;
+		int area = base * height / 2;
+		
+		System.out.println("Base: " + base);
+		System.out.println("Height: " + height);
+		System.out.println("Area: " + area);
+		
+		height = 4;
+		System.out.println("Base: " + base);
+		System.out.println("Height: " + height);
+		System.out.println("Area: " + area);
+	}
+
+}
