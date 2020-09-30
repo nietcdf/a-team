3c087451aab8238fbf0fdca1fd56dcf88fc0a91d
Rafal Jabrzemski <rjabrzemski@ou.edu>
2020-09-30 16:56:35 -0500

    Add an exception for the exceptional A-Team, so they catch up

---
+++ MyLengthException.java
@@ -0,0 +1,9 @@
+public class MyLengthException extends Exception{
+
+    private static final long serialVersionUID = 1L;
+
+    public MyLengthException(String info) {
+        super(info);
+    }
+    
+}
